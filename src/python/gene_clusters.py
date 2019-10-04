f95743926b5c6a464e024c97ba6ea41d5a3836d3
Ian Rambo <ian@Ians-MBP.dhcp.lbl.gov>
2019-10-04 11:55:42 -0700

    gene fetching

--- src/python/gene_clusters.py
+++ src/python/gene_clusters.py
@@ -36,6 +36,7 @@ def fetch_clusters(anchor_gff_df, gene_gff_df, gene_seq_dict, winsize, att_fs=';
         anchor_end = anchor_gff_df.at[i, 'end']
         anchor_id = anchor_gff_df.at[i, 'attributes'].str.split(att_fs).str[0].str.split('=').str[1].str.split('_').str[1]
 
+        print(anchor_source, anchor_start, anchor_end, anchor_id)
         gene_cluster_df = gene_gff_df[(gene_gff_df['source'] == anchor_source) & (gene_gff_df['start'] >= anchor_start - winsize) & (gene_gff_df['end'] <= anchor_end + winsize)]
         gene_cluster_df['gene_id'] = gene_cluster_df['source'].astype(str) + '_' + cluster_df['attributes'].str.split(att_fs).str[0].str.split('=').str[1].str.split('_').str[1]
 
