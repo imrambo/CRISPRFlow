94beb2642a677bb9be8368d6746aa49523066760
Ian Rambo <ian@Ians-MacBook-Pro.local>
2020-01-28 22:43:44 -0800

    pull crispr contigs

--- src/ccs/crispr_flow.py
+++ src/ccs/crispr_flow.py
@@ -140,7 +140,7 @@ try:
     crispr_array_df = gff3_to_pddf(gff = crispr_detect_gff, ftype = 'repeat_region', index_col=False)
     print(crispr_array_df.head())
     #Split up attributes for CRISPR arrays into new columns
-    crispr_array_df[['ID', 'Name', 'Parent', 'Repeat', 'Dbxref', 'OntologyTerm', 'ArrayQualScore']] = crispr_array_df['attributes'].str.replace('[A-Za-z]+\=', '', regex=True).str.split(pat = ";", expand=True)
+    crispr_array_df[['ID', 'Repeat', 'Dbxref', 'OntologyTerm', 'ArrayQualScore']] = crispr_array_df['attributes'].str.replace('[A-Za-z]+\=', '', regex=True).str.split(pat = ";", expand=True)
     #Select entries for spacers
     crispr_spacer_df = gff3_to_pddf(gff = crispr_detect_gff, ftype = 'binding_site', index_col=False)
     #Split up attributes for CRISPR spacers into new columns
